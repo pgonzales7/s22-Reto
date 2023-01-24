0b5ccc01715d2a1db669776dfa2c3940f8c8212d
pgonzales7 <p_gonzales_21@hotmail.com>
2023-01-23 21:38:36 -0500

    actualizado

--- src/app/principal/register/register.component.ts
+++ src/app/principal/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,25 +8,51 @@ import { Observable } from 'rxjs';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  firstname:string=""
-  lastname:string=""
-  username:string=""
-  password:string=""
   enviado=false;
-  
-  constructor() { }
-    EnviarMensaje(){
-      alert('Registro: ' + '\n ha sido enviado');
-      this.enviado = true;
-    }
-  SalirDeRuta():Observable<boolean> | boolean{
+
+  //para el formulario
+    constructor(private formBuilder: FormBuilder) { }
+
+  registroForm = this.formBuilder.group({
+      firstname:['', Validators.required],
+      lastname:['', Validators.required],
+      username:['', Validators.required],
+      password:['', Validators.required]
+  })
+
+  get firstname(){return this.registroForm.get('firstname')}
+  get lastname(){return this.registroForm.get('lastname')}
+  get username(){return this.registroForm.get('username')}
+  get password(){return this.registroForm.get('password')}
+
+
+  datos = new Array();
+  Enviar(){
+    this.datos.push({
+    'FirstName' : this.registroForm.get('firstname')?.value,
+    'LastName' : this.registroForm.get('lastname')?.value,
+    'Username' : this.registroForm.get('username')?.value,
+    'Password' : this.registroForm.get('password')?.value,
+    })
+    alert('Mensaje: '+`Nombre: ${this.firstname?.value}
+    Apellido: ${this.lastname?.value} Usuario: ${this.username?.value} Password: ${this.password?.value}`);
+    this.enviado=true;
+  }
+
+  nombre=this.firstname?.value;
+  apellido=this.lastname?.value;
+  usuario=this.username?.value;
+  contraseña=this.password?.value;
+
+
+  SalirDeRuta(): Observable<boolean> | boolean{
     
+    if(!this.nombre==undefined && !this.apellido==undefined && !this.usuario==undefined && !this.contraseña==undefined || this.enviado ){
+      return true;
+    }
 
-    if ((this.firstname != "" || this.lastname != ""|| this.username != ""|| this.password != "") &&  !this.enviado) {
-      const confirmar = confirm("¿Desea salir del Registro? \n Perderá todos los datos!!");
+      let confirmar = confirm("¿Desea abandonar el formulario? \n Perdera todos los datos!!");
       return confirmar;
-  }
+    }
 
-  return true;
-  }
 }
